cbc0558b1460acbcbc84099bbaf660fd686809db
Camila <camila.matosrocha1234@gmail.com>
2023-09-20 17:06:19 -0300

    Componente de like criado e acessibilidade ajustada

---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: ['./like-widget.component.scss']
+})
+export class LikeWidgetComponent implements OnInit {
+
+  constructor(
+    private uniqueIdService: UniqueIdService
+  ) { }
+
+  @Output() liked = new EventEmitter<void>();
+  @Input() likes = 0;
+  @Input() id = null;
+  public fonts = {
+    faThumbsUp
+  };
+
+  public ngOnInit(): void {
+    if (!this.id) {
+      this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like-widget');
+    }
+  }
+
+  public like(): void {
+    this.liked.emit();
+  }
+
+}
